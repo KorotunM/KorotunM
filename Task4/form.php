db977b2dba06a0e6c06d235ae18444792673c539
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-04-24 00:40:01 +0300

    Add files via upload

--- Task4/form.php
+++ Task4/form.php
@@ -42,7 +42,7 @@
       <select name="year" <?php if ($errors['year']) {print 'class="error"';} ?>>
         <?php
         for ($i = 1922; $i <= 2022; $i++) {
-          printf('<option value="%d">%d год</option>',$values['year'] == $i ? 'selected' : '', $i, $i);
+          printf('<option %s value="%d">%d год</option>',$values['year'] == $i ? 'selected' : '', $i, $i);
         }
         ?>
       </select><br />
@@ -52,7 +52,7 @@
       <select name="month"  <?php if ($errors['month']) {print 'class="error"';} ?> >
         <?php
         for ($i = 1; $i <= 12; $i++) {
-          printf('<option value="%d">%d месяц</option>',$values['month'] == $i ? 'selected' : '', $i, $i);
+          printf('<option %s value="%d">%d месяц</option>',$values['month'] == $i ? 'selected' : '', $i, $i);
         }
         ?>
       </select><br />
@@ -62,7 +62,7 @@
       <select name="day"  <?php if ($errors['day']) {print 'class="error"';} ?> >
         <?php
         for ($i = 1; $i <= 31; $i++) {
-          printf('<option value="%d">%d день</option>',$values['day'] == $i ? 'selected' : '', $i, $i);
+          printf('<option %s value="%d">%d день</option>',$values['day'] == $i ? 'selected' : '', $i, $i);
         }
         ?>
       </select>
