50d902749da33cb5ab2868bdd55bffb59e30b77c
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-26 15:14:05 +0300

    wellcome back!

--- Task6/index.php
+++ Task6/index.php
@@ -112,10 +112,10 @@ if($errors['check']){
     }
   }
   
-
+  if (!empty($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login'])) {
     include('ChangeAction_Form.php');
     printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']); 
-
+  }
 
   include('form.php');
 }
